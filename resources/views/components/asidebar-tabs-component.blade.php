7e0332e241a0446c5e45428745348cd4a4d68e4b
F0da <fadyemad687@gmail.com>
2026-06-19 02:58:47 +0300

    feat: enhance KPI card styles and add new metrics section to home page

--- resources/views/components/asidebar-tabs-component.blade.php
+++ resources/views/components/asidebar-tabs-component.blade.php
@@ -1,14 +1,13 @@
 <div>
     @foreach ($tabs as $key => $tab)
         <nav class="nav-section">
-            <div class="nav-label">{{ __(('keywords.' . $key)) }}</div>
-                @foreach ( $tab as $key => $value)
-                    <a class="nav-link" href="">
-                        {!! $value['svg'] !!}
-                        <span>{{ __(('keywords.' . $key)) }}</span>
-                    </a>
-                @endforeach
+            <div class="nav-label">{{ __('keywords.' . $key) }}</div>
+            @foreach ($tab as $key => $value)
+                <a class="nav-link" href="">
+                    {!! $value['svg'] !!}
+                    <span>{{ __('keywords.' . $key) }}</span>
+                </a>
+            @endforeach
         </nav>
     @endforeach
 </div>
-
--- resources/views/parts/aside.blade.php
+++ resources/views/parts/aside.blade.php
@@ -11,10 +11,10 @@
             <div class="brand-tag">v4.1.2 · preview</div>
         </div>
     </div>
-    
+
 
     <x-asidebar-tabs-component />
-  
+
     <div class="sidebar-footer">
         <div class="workspace">
             <div class="workspace-avatar">{{ Auth::user()->name[0] }}</div>
